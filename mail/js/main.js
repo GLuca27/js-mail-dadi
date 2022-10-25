87d3c6b3abdca620c892b047ae7b8a10a05a0256
GLuca27 <gianluca.sciglitano1@gmail.com>
2022-10-25 14:38:29 +0200

    nella console esce solo giuseppe

---
+++ mail/js/main.js
@@ -0,0 +1,10 @@
+const userMail = prompt("Inserisci il tuo nome aggiungendo @gmail.com");
+
+const iscritti = ("gianluca@gmail.com", "marco@gmail.com", "giuseppe@gmail.com");
+console.log(iscritti)
+if (prompt === iscritti){
+    alert ("L'email inserita è registrata")
+} else if(prompt != iscritti){
+    alert ("L'email inserita non è registrata")
+}
+
